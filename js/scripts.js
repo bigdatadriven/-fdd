678b936304edd456231885154b05dcb4b3a7bdb9
bigdatadriven <bigdatadriven@gmail.com>
2021-07-17 09:23:17 -0400

    add moosend subscribers

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 function adduser() {
-    var email = document.getElementById("useremail").value;
-    mootrack('identify', email, 'fdd');
+    const InputTextBox = document.getElementById("useremail");
+    mootrack('identify', InputTextBox.value, 'fdd');
+    const button = document.getElementById('submit-button');
+    const thanksDiv = document.getElementById('thanksMessage');
+    disableButton = true;
+    if (disableButton) {
+        button.disabled = true;
+        button.innerText = "Subscribed!";
+        InputTextBox.value = "";
+        thanksDiv.style = "padding-left: 500px; display: block"
+    } else { button.disabled = false; thanksDiv.style = "display: none" };
 }
