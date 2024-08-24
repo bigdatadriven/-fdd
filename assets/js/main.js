ddf7915a6d473e1115f7def50a3475a786f2027b
Ronen Ariely <ariely.ronen@gmail.com>
2024-08-24 10:30:37 +0300

    some editing and removing Show500px

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,7 +84,7 @@
   let selectHeader = select('#header')
   if (selectHeader) {
     const headerScrolled = () => {
-      if (window.scrollY > 100) {
+      if (window.scrollY > 50) {
         selectHeader.classList.add('header-scrolled')
       } else {
         selectHeader.classList.remove('header-scrolled')
